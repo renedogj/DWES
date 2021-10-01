9eb4f77fc80368d08f5b86d1df3331b07f5585da
unknown <renedogj@gmail.com>
2021-10-01 12:39:10 +0200

    ejbolsa.php

--- ejBolsa.php
+++ ejBolsa.php
@@ -1,18 +1,269 @@
 <?php
-	$Empresas = array (
-		"Inditex" => array(
-			"Precio"=>31.800,
-			"VariacionPorcentaje"=>-0.06,
-			"Variacion"=>-0.02,
-			"VolumenTitulos"=>482.599,
-			"Volumen"=>15306460.74
+	$datosGuardados = array(
+		"Precio",
+		"VariacionPorcentaje",
+		"Variacion",
+		"VolumenTitulos",
+		"Volumen"
+	);
+	$empresas = array (
+		"Inditex" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Aena" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Acciona" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Repsol" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Acs" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"BBVA" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"CaixaBank" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Santander" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"A" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"B" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"C" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"D" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"E" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"F" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"G" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"H" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"I" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"J" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"K" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"L" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"M" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"N" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Ñ" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"O" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"P" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Q" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"R" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"S" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"T" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"U" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"V" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"W" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"X" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Y" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
+		),
+		"Z" => array (
+			$datosGuardados[0]=>31.800,
+			$datosGuardados[1]=>-0.06,
+			$datosGuardados[2]=>-0.02,
+			$datosGuardados[3]=>482.599,
+			$datosGuardados[4]=>15306460.74
 		)
 	);
-
-	foreach($Empresas as $i => $i_value){
-		echo $i . "<br>";
-		foreach($Empresas[$i] as $j => $j_value){
-			echo $j . " => " . $j_value . "<br>";
-		}
+	echo "<table><tr>";
+	foreach($datosGuardados as $i){
+		echo "<th>" . $i . "</th>";
 	}
+	echo "</tr>";
+	foreach($empresas as $i => $i_value){
+		echo "<tr><th>" . $i . "</th>";
+		foreach($empresas[$i] as $j => $j_value){
+			echo "<td>" . $j_value . "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
 ?>
