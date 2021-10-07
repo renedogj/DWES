37975a5707e7c25361b34f3061e448d33a9fc6e2
unknown <renedogj@gmail.com>
2021-10-07 09:40:33 +0200

    ejbingo

---
+++ UT2/Ej Arrays/ej5a.php
@@ -0,0 +1,11 @@
+<?php
+	$array1 = array("Bases Datos", "Entornos Desarrollo", "Programación");
+	$array2 = array("Sistemas Informáticos","FOL","Mecanizado");
+	$array3 = array("Desarrollo Web ES","Desarrollo Web EC","Despliegue","Desarrollo Interfaces", "Inglés");
+	$arrayUnido1 = array();
+	for($i = 1; $i <= 3; $i++){
+		for($j = 0; $j < count($array$i);$j++){
+
+		}
+	}
+?>
